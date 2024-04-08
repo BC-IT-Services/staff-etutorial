a9cdce907590e946ae09770862cbfbba4eb94140
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-08 15:44:41 +0100

    Add files via upload

---
+++ acceptable-use-of-data-it-and-social-media-welsh/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://JKd3IU_Zm3bR4jw77DugW3NPdsTYz24I_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Defnydd Derbyniol o Ddata, TG a Chyfryngau Cymdeithasol"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Cynlluniwyd y rhaglen hon i&amp;#39;ch helpu i ddeall y Rheoliad Cyffredinol ar Ddiogelu Data (GDPR) a&amp;#39;r defnydd priodol o TG a chyfryngau cymdeithasol yng Ngholeg Penybont.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Yn sgil pwysigrwydd cynyddol diogelwch data a&amp;#39;r defnydd eang o dechnoleg yn ein bywydau bob dydd, mae&amp;#39;n hanfodol bod gan bawb ddealltwriaeth glir o&amp;#39;r gyfraith a&amp;#39;r canllawiau hyn. Bydd yr hyfforddiant yma&amp;rsquo;n darparu&amp;rsquo;r wybodaeth a&amp;rsquo;r sgiliau sydd eu hangen arnoch i lywio byd cymhleth GDPR a defnyddio technoleg a chyfryngau cymdeithasol mewn modd cyfrifol a moesegol yn ystod eich amser yng Ngholeg Penybont.&lt;/p&gt;"
+};
