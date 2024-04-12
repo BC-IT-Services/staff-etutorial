2fcc62fbd07852217390978cafdc6cd08d6ed051
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-12 13:58:20 +0100

    Add files via upload

---
+++ equality-diversity-and-inclusion-welsh/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://b56ECL6qEjDrkbf_AGfRtxKyrcncumd1_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Cydraddoldeb, Amrywiaeth a Chynhwysiant"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p style=&#34;text-align: justify;&#34;&gt;&lt;strong&gt;Datganiad Cenhadaeth ar gyfer Cydraddoldeb, Amrywiaeth a Chynhwysiant&lt;/strong&gt;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;&amp;ldquo;Mae Coleg Penybont yn lle arbennig. Mae trin pobl yn deg, gyda pharch, a gwerthfawrogi gwahaniaeth yn bwysig iawn inni. Mae ein gwerthoedd, sy&amp;rsquo;n deillio o&amp;rsquo;n cenhadaeth, Byddwch Bopeth y Gallwch Fod, yn seiliedig ar hyrwyddo amrywiaeth a chyfleoedd cyfartal, ac ar gefnogi pob myfyriwr ac aelod staff i fod y gorau y gallant fod. Credwn fod sefydliad sy&amp;rsquo;n dathlu amrywiaeth yn sefydliad gwell ac yn sefydliad sydd, yn ei dro, yn cyfoethogi bywyd yn y coleg i bawb.&amp;rdquo;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;&lt;strong&gt;Ein Gwerthoedd&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;Mae ein cenhadaeth yn syml &amp;ndash; &lt;strong&gt;byddwch bopeth y gallwch fod.&lt;/strong&gt; Byddwch chi&amp;rsquo;ch hun.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Rydyn ni am greu amgylchedd &lt;strong&gt;cynhwysol&amp;nbsp;&lt;/strong&gt;sy&amp;rsquo;n galluogi pobl i fod yn arloesol ac yn &lt;strong&gt;angerddol&amp;nbsp;&lt;/strong&gt;am yr hyn maen nhw&amp;rsquo;n ei wneud. Rydyn ni am greu cyfleoedd &lt;strong&gt;ysbrydoledig&amp;nbsp;&lt;/strong&gt;sy&amp;rsquo;n trawsnewid bywydau a dathlu llwyddiannau pob unigolyn. Rydyn ni wedi ymrwymo i wneud penderfyniadau sy&amp;rsquo;n foesegol ac yn &lt;strong&gt;bobl-ganolog&lt;/strong&gt;, er lles pob unigolyn ac ym mhob rhyngweithiad.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Rydyn ni&amp;rsquo;n gwybod mai&amp;rsquo;r safonau rydyn ni&amp;rsquo;n cerdded heibio iddynt yw&amp;rsquo;r safonau rydyn ni&amp;rsquo;n eu derbyn, felly trwy fod yn &lt;strong&gt;chwarawr t&amp;icirc;m&lt;/strong&gt; sy&amp;rsquo;n ymroddedig i&amp;rsquo;n gwerthoedd sefydliadol, gallwn sicrhau bod pawb yn &lt;strong&gt;barod &lt;/strong&gt;i ddysgu a gweithio mewn amgylchedd lle maen nhw&amp;rsquo;n cael eu &lt;strong&gt;parchu &lt;/strong&gt;ac yn teimlo&amp;rsquo;n &lt;strong&gt;ddiogel&lt;/strong&gt;, i &lt;strong&gt;fod yn bopeth y gallant fod&lt;/strong&gt;.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;"
+};
