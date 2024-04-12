99e40af38f371f2c82dbb9b8a3088a446fb82ef5
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-12 14:53:47 +0100

    Add files via upload

---
+++ acceptable-use-of-data-it-and-social-media-english/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://tkqgNGFx_u_kvmczE9R4Drtx8WN7KJqQ_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Acceptable Use of Data, IT and Social Media"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p style=&#34;text-align: justify;&#34;&gt;This program is designed to help you understand the General Data Protection Regulation (GDPR) and the appropriate use of IT and social media at Bridgend College.&amp;nbsp;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&#34;text-align: justify;&#34;&gt;With the increasing importance of data security and the widespread use of technology in our daily lives, it&amp;#39;s essential that everyone has a clear understanding of these laws and guidelines. This training will provide you with the knowledge and skills needed to navigate the complex world of GDPR and use technology and social media in a responsible and ethical manner during your time at Bridgend College.&amp;nbsp;&lt;/p&gt;"
+};
