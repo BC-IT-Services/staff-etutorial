9819ecdcd078402a58733003c7a7802a8c3b88e0
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-19 11:33:58 +0100

    Add files via upload

---
+++ workplace-health-and-safety-welsh/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://nfp_L7JPrR7H5r3lDOcOy3dA6YdNyg_y_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Gweithio’n Ddiogel – Cyflwyniad i Iechyd a Diogelwch yn y Gweithle"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Mae gan bob cyflogai hawl i weithio mewn amgylchedd diogel a ddarperir gan ei gyflogwr.&lt;/p&gt;&lt;p&gt;Fel cyflogai, mae hefyd yn gyfrifoldeb arnoch chi i ofalu am eich iechyd a&amp;rsquo;ch diogelwch eich hun, ac i beidio &amp;acirc; rhoi eich cydweithwyr mewn perygl.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Mae gweithio&amp;rsquo;n ddiogel yn golygu cydnabod bod risgiau&amp;rsquo;n bresennol mewn unrhyw amgylchedd gwaith.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Os ydych yn gweithio yn y diwydiant adeiladu, gallech fod mewn perygl o gwympo, neu o ddod i gysylltiad &amp;acirc; sylweddau peryglus fel asbestos. A fyddech chi&amp;rsquo;n gallu adnabod y peryglon hyn? Ydych chi wedi gwneud yr hyfforddiant angenrheidiol i weithio&amp;rsquo;n ddiogel?&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Mae gweithwyr swyddfa hefyd yn agored i risgiau, e.e. os ydynt yn defnyddio eu sgriniau arddangos mewn modd amhriodol. Gallant roi eu hunain ac eraill mewn perygl trwy beidio &amp;acirc; dilyn gweithdrefnau diogelwch t&amp;acirc;n cywir. Fel cyflogai, os ydych chi&amp;rsquo;n gallu adnabod y peryglon hyn, gallwch amddiffyn eich hun a&amp;#39;ch cydweithwyr rhag niwed. Mae angen hefyd i chi gydweithredu &amp;acirc;&amp;#39;ch cyflogwr, sydd &amp;acirc; gweithdrefnau a pholis&amp;iuml;au i&amp;#39;ch cadw&amp;#39;n ddiogel.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Yn y cwrs hwn, byddwch yn dysgu:&amp;nbsp;&lt;/p&gt;&lt;ol&gt;&lt;li&gt;Canlyniadau diystyru iechyd a diogelwch&amp;nbsp;&lt;/li&gt;&lt;li&gt;Y gyfraith ynghylch iechyd a diogelwch&amp;nbsp;&lt;/li&gt;&lt;li&gt;Cyfrifoldeb eich cyflogwr&lt;/li&gt;&lt;li&gt;Eich cyfrifoldeb chi fel cyflogai&lt;/li&gt;&lt;li&gt;Meddwl yn ddiogel ac atal damweiniau&lt;/li&gt;&lt;/ol&gt;"
+};
