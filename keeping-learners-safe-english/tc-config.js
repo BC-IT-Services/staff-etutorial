42bf4f6a1df6e023f701a1db39f4ba5d58bac22e
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-23 15:19:10 +0100

    Add files via upload

---
+++ keeping-learners-safe-english/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://BT8_YLHIpYIIdsF_gSGVQr9VNP2Fc6xD_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Keeping Learners Safe"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
---
+++ keeping-learners-safe-welsh/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://K1si8tcsHOFGe021SoodoqGMqtcuCBjm_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Cadw Dysgwyr yn Ddiogel"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
