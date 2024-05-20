00ab653172f7583cdbf5f6828338414b2dad02d4
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-05-20 13:53:12 +0100

    Add files via upload

---
+++ prevent-english/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://mwxFZ5Yxb6iN4Qvy5asgFRIW93trPhL__rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Prevent"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;strong&gt;Bridgend College&lt;/strong&gt; seeks to protect its staff and students against radicalisation and extremism. This course will enable you to understand the &lt;strong&gt;Government Counter Terrorism Strategy&lt;/strong&gt; - &lt;strong&gt;Prevent&lt;/strong&gt;. This strategy aims to help people who are at risk of becoming terrorists or supporting terrorism.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;strong&gt;Prevent&amp;nbsp;&lt;/strong&gt;&lt;/strong&gt;sits alongside long-established safeguarding duties and is part of the Counter-Terrorism Strategy meaning that specified authorities such as education, health, local authorities, police and criminal justice agencies (prisons, probation, youth justice) have a duty to help prevent people from becoming terrorists or supporting terrorism.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;By completing the course, you will understand the root cause of radicalisation, extremism and what to do if you come across any concerning behaviours.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Bridgend College&lt;/strong&gt; will fulfil its responsibilities as outlined by the &lt;strong&gt;Prevent Duty Guidance&lt;/strong&gt;, for further education institutions in England and Wales.&lt;/p&gt;"
+};
