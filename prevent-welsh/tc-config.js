2a37307675938f339afaed1231caa199e0ac8da4
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-05-20 13:51:52 +0100

    Add files via upload

---
+++ prevent-welsh/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://C2Vl-LPtC6V-kcaE-uATTaL_rFY3r0Ai_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Dyletswydd"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Mae &lt;strong&gt;Coleg Penybont&lt;/strong&gt; yn ymdrechu i amddiffyn ei staff a&amp;#39;i fyfyrwyr rhag radicaleiddio ac eithafiaeth. Bydd y cwrs hwn yn eich galluogi i ddeall &lt;strong&gt;Strategaeth Gwrthderfysgaeth y Llywodraeth &lt;/strong&gt;- &amp;#39;&lt;strong&gt;Prevent&lt;/strong&gt;&amp;#39;. Nod y strategaeth hon yw helpu pobl sydd mewn perygl o ddod yn derfysgwyr neu gefnogi terfysgaeth i&amp;#39;w hatal rhag gwneud hynny.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Mae &lt;strong&gt;&lt;strong&gt;Prevent&amp;nbsp;&lt;/strong&gt;&lt;/strong&gt;yn sefyll ochr yn ochr &amp;acirc; dyletswyddau diogelu eraill sydd wedi&amp;rsquo;u hen sefydlu ac mae&amp;rsquo;n rhan o&amp;rsquo;r Strategaeth Gwrthderfysgaeth sy&amp;rsquo;n golygu bod gan awdurdodau mewn sectorau penodol fel addysg, iechyd, awdurdodau lleol, yr heddlu ac asiantaethau cyfiawnder troseddol (carchardai, y gwasanaeth prawf, cyfiawnder ieuenctid) ddyletswydd i helpu i atal pobl rhag dod yn derfysgwyr neu gefnogi terfysgaeth.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Drwy gwblhau&amp;rsquo;r cwrs, byddwch yn dod i ddeall yr hyn sy&amp;#39;n achosi radicaleiddio ac eithafiaeth, a beth i&amp;rsquo;w wneud os ydych yn dod ar draws unrhyw ymddygiadau sy&amp;rsquo;n peri pryder.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Bydd &lt;strong&gt;Coleg Penybont&lt;/strong&gt; yn cyflawni ei gyfrifoldebau yn unol &amp;acirc; &lt;strong&gt;Chanllawiau&amp;#39;r Ddyletswydd Prevent&amp;nbsp;&lt;/strong&gt;ar gyfer sefydliadau addysg bellach yng Nghymru a Lloegr.&lt;/p&gt;"
+};
